7000761993b972cc1ea21a43627f9d664b79ce2d
Salt-N-Pepacls <exemplo@exemplo.com>
2020-05-31 19:29:52 -0300

    Adicionando variavel de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://backend-amigo-chocolate.herokuapp.com",
+    baseURL: procees.env.REACT_APP_API_URL,
 })
 
 export default api;
